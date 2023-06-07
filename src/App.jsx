4d0ca4640f3406a8355ba05522faf1e35d3b4269
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-07 14:39:56 +1000

    R

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import './App.css';
 import Todo from './components/Todo.jsx';
-
+import Title from './components/Title.jsx';
+import Modal from './components/Modal.jsx';
 function App() {
   return (
-    <div>
-    <h1>my To Do List</h1>
-    <div>
-      <h2>Finish front end Road Map</h2>
-      <button>Delete</button>
-    </div><div>
-      <h2>Finish the interview section</h2>
-      <button>Delete</button>
-    </div><div>
-      <h2>Landa  a Job</h2>
-      <button>Delete</button>
-    </div>
-    </div>
+      <div>
+      <Title />
+      <div className="todo__wrapper">
+      <Todo />
+      <Todo />
+      <Todo />
+      </div>
+      <Modal />
+      </div>
     
   );
 }
