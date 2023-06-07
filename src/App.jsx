b44f24c64b451dc0ca21f6f02d26b2334f9fbf70
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-07 13:48:27 +1000

    Prc

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Todo from './components/Todo.jsx';
+
+function App() {
+  return (
+    <div>
+    <h1>my To Do List</h1>
+    <div>
+      <h2>Finish front end Road Map</h2>
+      <button>Delete</button>
+    </div><div>
+      <h2>Finish the interview section</h2>
+      <button>Delete</button>
+    </div><div>
+      <h2>Landa  a Job</h2>
+      <button>Delete</button>
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
