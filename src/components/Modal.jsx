7a246b08e2b06c2b44583352a77fbcb4312b8b9d
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-10 19:39:42 +1000

    Re

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,15 @@ import './Modal.css'
 function Modal() {
     returm (
         <div>
-  
+  <div className="modal">
+    <p className="modal__title">Are you sure?</p>
+    <div className="modal__buttons">
+      <button className="btn btn__cancel">Cancel</button>
+      <button className="btn">Confirm</button>
+    </div>
+  </div>
+  <div className="backdrop" />
+</div>
     )
 }
 
